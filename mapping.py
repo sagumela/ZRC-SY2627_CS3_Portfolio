47b0ace78c061277edd74aa44e8628918689c448
sagumela <sagumela@zrc.pshs.edu.ph>
2026-09-22 21:25:59 +0800

    Student and Course classes

---
+++ mapping.py
@@ -0,0 +1,24 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+
+class Course:
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+
+    def add_student(self, student):
+        self.students.append(student)
+
+student1 = Student("Amara")
+student2 = Student("Xadri")
+
+course = Course("Computer Science")
+
+course.add_student(student1)
+course.add_student(student2)
+
+print(course.name)
+for student in course.students:
+    print(student.name)
